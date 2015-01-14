4fb58d711f6566d91b05169a84da0a838f5323c3
David Aramant <aramant@sep.com>
2015-01-14 14:34:50 -0500

    Switched example to Java's planet info; added instance name and ToString

---
+++ RichEnumsExample/RichEnumsExample/Planet.Custom.cs
@@ -0,0 +1,18 @@
+﻿namespace RichEnumsExample
+{
+    public partial class Planet
+    {
+        // universal gravitational constant  (m3 kg-1 s-2)
+        public const double G = 6.67300E-11;
+
+        public double SurfaceGravity()
+        {
+            return G * _mass / (_radius * _radius);
+        }
+
+        public double SurfaceWeight(double otherMass)
+        {
+            return otherMass * SurfaceGravity();
+        }
+    }
+}
--- RichEnumsExample/RichEnumsExample/Program.cs
+++ RichEnumsExample/RichEnumsExample/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-namespace RichEnumsExample {
-	class MainClass {
-		public static void Main( string[] args ) {
-			Console.WriteLine( "Hello World!" );
-		}
-	}
+namespace RichEnumsExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double earthWeight = 85;
+            double mass = earthWeight / Planet.Earth.SurfaceGravity();
+
+            foreach (var p in Planet.GetAll())
+            {
+                Console.WriteLine("Weight on {0} is {1}",
+                             p, p.SurfaceWeight(mass));
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
 }
